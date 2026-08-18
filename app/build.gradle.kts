9fe77482455827765cbf20a88f794adb0d16aabd
Ruslan Mirkhusainov <v-rmirkhusainov@expediagroup.com>
2026-08-18 15:03:26 +0200

    FC-13: transaction screen added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.material)
     implementation(libs.androidx.navigation.compose)
 
     implementation(libs.androidx.appcompat)
--- app/src/main/kotlin/com/financetracker/feature/home/ui/HomeScreen.kt
+++ app/src/main/kotlin/com/financetracker/feature/home/ui/HomeScreen.kt
@@ -1,7 +1,12 @@
 package com.financetracker.feature.home.ui
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
@@ -31,11 +37,24 @@ fun HomeScreen(
         }
     }
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
-        Button(onClick = viewModel::onLogoutClick) {
+        Button(
+            onClick = { navController.navigate(Destination.Transactions.route) },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(stringResource(R.string.label_view_transactions))
+        }
+        Spacer(Modifier.height(8.dp))
+        Button(
+            onClick = viewModel::onLogoutClick,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
             Text(stringResource(R.string.label_logout))
         }
     }
